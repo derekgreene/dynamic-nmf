ea222dc35d793439e7778cb836e724a784979b8f
Derek Greene <derek.greene@ucd.ie>
2015-11-08 20:06:01 +0000

    Wrap term lists to improve readability

--- display-topics.py
+++ display-topics.py
@@ -18,6 +18,8 @@ def main():
 	if( len(args) < 1 ):
 		parser.error( "Must specify at least one topic modeling results file produced by NMF" )
 	log.basicConfig(level=20, format='%(message)s')
+	# number of columns to use when displaying topics
+	column_size = 8
 
 	# Load each cached ranking set
 	for in_path in args:
@@ -25,7 +27,14 @@ def main():
 		log.info( "- Loaded model with %d topics from %s" % (len(term_rankings), in_path) )
 		log.info( "Top %d terms for %d topics:" % (options.top,len(term_rankings)) )
 		m = unsupervised.rankings.term_rankings_size( term_rankings )
-		print unsupervised.rankings.format_term_rankings( term_rankings, labels, min(options.top,m) )
+		# wrap columns to improve readability
+		current = 0
+		while current < len(term_rankings):
+			current_end = min(current+column_size,len(term_rankings))
+			current_rankings = term_rankings[current:current_end]
+			current_labels = labels[current:current_end]
+			print( unsupervised.rankings.format_term_rankings( current_rankings, current_labels, min(options.top,m) ) )
+			current += column_size
 
 # --------------------------------------------------------------
 
