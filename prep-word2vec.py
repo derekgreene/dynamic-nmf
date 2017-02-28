8fff4b479d9878a87c2a42768c4ab0cf6c2f4d8e
Derek Greene <derek.greene@ucd.ie>
2017-02-28 09:40:38 +0000

    Allow eaiser selection of word2vec parameters

--- prep-word2vec.py
+++ prep-word2vec.py
@@ -18,9 +18,10 @@ def main():
 	parser.add_option("-s", action="store", type="string", dest="stoplist_file", help="custom stopword file path", default=None)
 	parser.add_option("--df", action="store", type="int", dest="min_df", help="minimum number of documents for a term to appear", default=10)
 	parser.add_option("--minlen", action="store", type="int", dest="min_doc_length", help="minimum document length (in characters)", default=50)
-	parser.add_option("--size", action="store", type="int", dest="w2v_size", help="the dimensionality of the Word2Vec feature vectors", default=100)
+	parser.add_option("-d","--dimensions", action="store", type="int", dest="dimensions", help="the dimensionality of the word vectors", default=100)
 	parser.add_option("--window", action="store", type="int", dest="w2v_window", help="the maximum distance for Word2Vec to use between the current and predicted word within a sentence", default=5)
 	parser.add_option("-o","--outdir", action="store", type="string", dest="dir_out", help="output directory (default is current directory)", default=None)
+	parser.add_option("-m", action="store", type="string", dest="model_type", help="type of word embedding model to build (sg or cbow)", default="cbow")
 	# Parse command line arguments
 	(options, args) = parser.parse_args()
 	if( len(args) < 1 ):
@@ -43,9 +44,15 @@ def main():
 	docgen = text.util.DocumentTokenGenerator( args, options.min_doc_length, stopwords )
 
 	# Build the Word2Vec model from the documents that we have found
-	log.info( "Building Word2vec model..." )
-	model = gensim.models.Word2Vec(docgen, size=options.w2v_size, min_count=options.min_df, window=options.w2v_window, workers=4)
-	log.info( "Build model: %s" % model )
+	log.info( "Building Word2vec %s model..." % options.model_type )
+	if options.model_type == "cbow":
+		model = gensim.models.Word2Vec(docgen, size=options.dimensions, min_count=options.min_df, window=options.w2v_window, workers=4, sg = 0)
+	elif options.model_type == "sg":
+		model = gensim.models.Word2Vec(docgen, size=options.dimensions, min_count=options.min_df, window=options.w2v_window, workers=4, sg = 1)
+	else:
+		log.error("Unknown model type '%s'" % options.model_type )
+		sys.exit(1)
+	log.info( "Built model: %s" % model )
 
 	# Save the Word2Vec model
 	model_out_path = os.path.join( dir_out, "w2v-model.bin" )
